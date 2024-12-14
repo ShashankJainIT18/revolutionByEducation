f30af1ab7f097f32b5ddee2ec759ba3df083368d
ShashankJainIT18 <68013275+ShashankJainIT18@users.noreply.github.com>
2024-12-14 15:05:05 +0530

    added dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,11 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("org.springframework:spring-web:6.2.1")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.4.0")
+	compileOnly("org.projectlombok:lombok:1.18.36")
+	implementation("com.mysql:mysql-connector-j:9.1.0")
+
 }
 
 tasks.withType<Test> {
